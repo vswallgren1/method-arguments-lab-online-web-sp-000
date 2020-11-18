dce4c239af5072ac814c040ebc03df29e2bdc023
Vince W <vincewallgren@me.com>
2020-11-18 05:06:43 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,6 @@
 def introduction(name)
-  name = "Josh"
   puts "Hi, my name is #{name}"
 end
 def introduction_with_language(name, language)
-  language = "Ruby"
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
